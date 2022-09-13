968befcbbc8a16ac95f906ea46058cfcfb438aad
Refun <refunfk@gmail.com>
2022-09-13 18:29:22 +0530

    updation in registration.html and web.py -> added flask flash for username email redundancy checking in backend and passed pop message in html page ->created users table to get registration data

---
+++ web.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template,request,flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
+from sqlalchemy.sql import func
+from flask_login import UserMixin
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "helloworld"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blog'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+    
+    
+class User(db.Model,UserMixin):
+    """User account model."""
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    
+    
+@app.route("/", methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        email_exists = User.query.filter_by(email=email).first()
+        username_exists = User.query.filter_by(username=username).first()
+
+        if email_exists:
+            flash('Email is already in use.', category='error')
+        elif username_exists:
+            flash('Username is already in use.', category='error')
+            
+
+        else:
+            
+            new_user = User(email=email, username=username, password=generate_password_hash(
+                password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('User created!')
+
+    return render_template("registration.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
