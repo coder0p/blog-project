9e9a69621bbf85a0a3b6824a625246ca00e0cb39
Refun <refunfk@gmail.com>
2022-09-15 23:47:58 +0530

    added base.html & align all stuff properly with the use of block's -AND- fixed some bugs

--- website/__init__.py
+++ website/__init__.py
@@ -19,7 +19,6 @@ def create_app():
 
     from .models import User
 
- #   create_database(app)
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
@@ -31,7 +30,3 @@ def create_app():
 
     return app
 
-# def create_database(app):
-#     if not path.exists("website/" + DB_NAME):
-#         db.create_all(app=app)
-#         print("Created database!")
