7246f9a59fc408242693ad0c1f6cd807058c99a5
Aldrin Thomas <aldrinthomas300@gmail.com>
2022-09-16 17:21:45 +0530

    post template update
    * modified home.html : set url_for in POST button in navbar, set url_for in POST in no user logged in session
    * modified post.html : implemented template for posts.html
    * modified view.py : set views route for post

--- website/views.py
+++ website/views.py
@@ -9,3 +9,9 @@ views = Blueprint("views", __name__)
 #@login_required
 def home():
     return render_template("home.html",current_user=current_user)
+
+
+@views.route("/post")
+@login_required
+def post():
+    return render_template("posts.html",current_user=current_user)
