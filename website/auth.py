9c9c02c5da56cbdfee077305fcb3fc8fce187e4b
Aldrin Thomas <aldrinthomas300@gmail.com>
2022-09-18 20:51:18 +0530

    code refactor and added a feature to fade out flask mesasge

--- website/auth.py
+++ website/auth.py
@@ -16,7 +16,7 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash("Logged in successfully !", category='success')
+                flash("Logged in successfully..", category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
@@ -34,21 +34,23 @@ def sign_up():
         username = request.form.get("username")
         password1 = request.form.get("password")
         password2 = request.form.get("repeat_password")
-
+        
 
         email_exists = User.query.filter_by(email=email).first()
         username_exists = User.query.filter_by(username=username).first()
 
         if email_exists:
-            flash('Email is already in use.', category='error')
+            flash('Email is already in use!', category='error')
+        elif email == "":
+            flash("Email shouldn't be empty!", category='error')
         elif username_exists:
-            flash('Username is already in use.', category='error')
+            flash('Username is already in use!', category='error')
         elif password1 != password2:
             flash('Password don\'t match!', category='error')
         elif len(username) < 2:
-            flash('Username is too short.', category='error')
+            flash('Username is too short!', category='error')
         elif len(password1) <= 6:
-            flash('Password is too short.', category='error')
+            flash('Password is too short!', category='error')
 
 
         else:
@@ -67,5 +69,5 @@ def sign_up():
 @login_required
 def logout():
     logout_user()
-    flash('logout successfully',category='success')
+    flash('logout successfully..',category='success')
     return redirect(url_for("views.home"))
