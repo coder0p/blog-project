7b54912de82477ea403a9845f0a0498af49ff06e
Refun <refunfk@gmail.com>
2022-09-25 17:01:50 +0530

    like implemented without reloading the page

--- website/static/index.js
+++ website/static/index.js
@@ -83,3 +83,24 @@ function categoryInput(){
 
 
 
+function like(postId) {
+  const likeCount = document.getElementById(`likes-count-${postId}`);
+  console.log(likeCount)
+  const likeButton = document.getElementById(`like-button-${postId}`);
+  console.log(likeButton)
+
+ 
+  fetch(`/likepost/${postId}`, { method: "POST" })
+    .then((res) => res.json())
+    .then((data) => {console.log(data)
+      likeCount.innerHTML = data["likes"];
+      if (data["liked"] === true) {
+        likeButton.className = "fas fa-thumbs-up";
+      } else {
+        likeButton.className = "far fa-thumbs-up";
+      }
+    })
+}
+
+
+
