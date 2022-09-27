94d4a4f9d95dc9c747fa6ea9975168deaf16a6e3
gopika <gopikasritha1997@gmail.com>
2022-09-27 11:10:41 +0530

    added breadcrumb
    breadcrumb in post page navigation to home page

--- website/views.py
+++ website/views.py
@@ -1,4 +1,3 @@
-from turtle import pos
 from flask import Blueprint, render_template, request, flash, redirect, url_for,jsonify
 from flask_login import login_required, current_user
 from .models import Post,Comment,Category,Like
